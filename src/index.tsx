b63020250741e999e4aae564cccb022c39ef4682
Uzi Cohen <uzic@wix.com>
2022-10-17 16:52:23 +0300

    Remove -

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,10 @@ interface Props {
   style?: any;
 }
 
+function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 const CountryFlag = ({ isoCode, size, style }: Props) => {
   // This switch case is just there because you can't name variables "in" and "do"
   switch (isoCode.toLowerCase()) {
@@ -16,6 +20,14 @@ const CountryFlag = ({ isoCode, size, style }: Props) => {
       break;
     case "do":
       return <Image source={flag["dom"]} style={[{ width: size * 1.6, height: size }, style]} />;
+      break;
+    case "gb-eng":
+    case "gb-nir":
+    case "gb-sct":
+    case "gb-wls":
+      const suffix = capitalizeFirstLetter(isoCode.toLowerCase().split('-')[1]);
+      const key = `gb${suffix}`;
+      return <Image source={flag[key]} style={[{ width: size * 1.6, height: size }, style]} />;
       break;
     default:
       return (
